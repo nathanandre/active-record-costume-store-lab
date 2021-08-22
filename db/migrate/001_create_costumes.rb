b5a38545824eb97391f09747da244ecdd6036750
nathanandre <nathanandreburns@gmail.com>
2021-08-22 19:24:34 -0400

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,4 +1,4 @@
-class CreateCostunes < ActiveRecord::Migration[5.2]
+class CreateCostumes < ActiveRecord::Migration[5.2]
     def change 
         create_table :costumes do |t|
             t.string :name
